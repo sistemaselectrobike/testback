03f3ea7fbcf25c550055f3c574ec1a97c1fdc4c7
sistemaselectrobike <sistemas@electrobike.com.mx>
2016-06-23 11:11:35 -0500

    Cambios

---
+++ www/js/index.js
@@ -0,0 +1,18 @@
+log('App started: ' + new Date());
+
+document.addEventListener('deviceready', function() {
+  log('deviceready. resumeType=' + cordova.backgroundapp.resumeType);
+  initCallbacks();
+  // Don't bother drawing UI when running in the background.
+  if (cordova.backgroundapp.resumeType == 'launch') {
+    renderUi('initial launch');
+  } else { // resumeType == ''
+    log('Running in the background!');
+  }
+});
+document.addEventListener('resume', function() {
+  log('resume event. resumeType=' + cordova.backgroundapp.resumeType);
+  if (cordova.backgroundapp.resumeType == 'normal-launch') {
+    renderUi('user launch when backgrounded');
+  }
+});
