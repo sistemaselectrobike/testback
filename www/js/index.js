0899e1e38b42b1e53cd0c8629535564c65071302
sistemaselectrobike <sistemas@electrobike.com.mx>
2016-06-23 16:13:03 -0500

    test

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,21 @@
 log('App started: ' + new Date());
 
-
+document.addEventListener('deviceready', function() {
+  log('deviceready. resumeType=' + cordova.backgroundapp.resumeType);
+  initCallbacks();
+  // Don't bother drawing UI when running in the background.
+  if (cordova.backgroundapp.resumeType == 'launch') {
+    renderUi('initial launch');
+  } else { // resumeType == ''
+    log('Running in the background!');
+  }
+});
+document.addEventListener('resume', function() {
+  log('resume event. resumeType=' + cordova.backgroundapp.resumeType);
+  if (cordova.backgroundapp.resumeType == 'normal-launch') {
+    renderUi('user launch when backgrounded');
+  }
+});
 
 //setTimeout(function(){ createNotification("Hola mundo desde la app"); navigator.vibrate(500); }, 5000);
 
@@ -8,37 +23,20 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-
-        document.addEventListener('deviceready', function() {
-          log('deviceready. resumeType=' + cordova.backgroundapp.resumeType);
-          initCallbacks();
-          // Don't bother drawing UI when running in the background.
-          if (cordova.backgroundapp.resumeType == 'launch') {
-            renderUi('initial launch');
-          } else { // resumeType == ''
-            log('Running in the background!');
-          }
-        });
-        document.addEventListener('resume', function() {
-          log('resume event. resumeType=' + cordova.backgroundapp.resumeType);
-          if (cordova.backgroundapp.resumeType == 'normal-launch') {
-            renderUi('user launch when backgrounded');
-          }
-        });
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      app.receivedEvent('deviceready');
+        app.receivedEvent('deviceready');
     },
-    onDeviceResume: function(){
-
-    }
-    ,
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
